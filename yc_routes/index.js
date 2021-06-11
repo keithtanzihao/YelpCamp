b1b08feed04bc99190ba59adb2fa95faf18600ff
keithtanzihao <keithtanzihao@gmail.com>
2021-06-11 23:20:46 +1000

    Heroku Deployment: Replaced /home route with /

--- yc_routes/index.js
+++ yc_routes/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 
 // NOTE: Order of route scirpts seems to make a difference
-router.use('/home', require('./r_home'));
+router.use('/', require('./r_home'));
 router.use('/yelpCamp', require('./r_campground'));
 router.use('/yelpCamp', require('./r_reviews'));
 router.use('/', require('./r_user'));
